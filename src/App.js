3880bff80bddc59cbedb46697a9ef2863cea3180
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-04 13:05:47 -0700

    Got my list of tasks made and will start getting it rendered to the page to be manipulated.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,46 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+
+const menialTasks = [
+  {
+    task: "Fetch Some Item",
+    id: 101,
+    endured: false
+  },
+  {
+    task: "Discipline Underling",
+    id: 102,
+    endured: false
+  },
+  {
+    task: "Watch Door",
+    id: 103,
+    endured: false
+  },
+  {
+    task: "Watch Prisoner",
+    id: 104,
+    endured: false
+  },
+  {
+    task: "Grovel",
+    id: 105,
+    endured: false
+  },
+  {
+    task: "Take Lashings",
+    id: 106,
+    endured: false
+  }
+];
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      menialTasks // same as menialTasks:menialTasks
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
